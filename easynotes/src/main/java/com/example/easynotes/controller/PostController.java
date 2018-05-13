3bc377e92a6d1045a49f4be6a421e8bf30fb603d
unknown <maivanbao@gmail.com>
2018-05-13 22:31:05 +0700

    update PostController

--- easynotes/src/main/java/com/example/easynotes/controller/PostController.java
+++ easynotes/src/main/java/com/example/easynotes/controller/PostController.java
@@ -2,6 +2,8 @@ package com.example.easynotes.controller;
 
 import javax.validation.Valid;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -18,9 +20,10 @@ import com.example.easynotes.Exception.ResourceNotFoundException;
 import com.example.easynotes.model.Post;
 import com.example.easynotes.repository.PostRepository;
 
+
 @RestController
 public class PostController {
-
+	private static final Logger logger = LogManager.getLogger(PostController.class);
 	@Autowired
 	PostRepository postRepository;
 
@@ -30,38 +33,33 @@ public class PostController {
 
 	}
 
+	@GetMapping("/posts/{postId}")
+	public Post getPostById(@PathVariable(value = "postId") Long postId) {
+    logger.info("Info log");
+		return postRepository.findById(postId).orElseThrow(() -> new ResourceNotFoundException("Post " + postId + " not found"));
+	}
+
 	@PostMapping("/posts")
 	public Post creatPost(@Valid @RequestBody Post post) {
 		return postRepository.save(post);
 	}
 
 	@PutMapping("/posts/{postId}")
-	public Post updatePost(@PathVariable Long postId,
-			@Valid @RequestBody Post postRequest) {
-		return postRepository
-				.findById(postId)
-				.map(post -> {
-					post.setTitle(postRequest.getTitle());
-					post.setContent(postRequest.getContent());
-					post.setDescription(postRequest.getDescription());
-					return postRepository.save(post);
-				})
-				.orElseThrow(
-						() -> new ResourceNotFoundException("Post " + postId
-								+ " not found"));
+	public Post updatePost(@PathVariable Long postId, @Valid @RequestBody Post postRequest) {
+		return postRepository.findById(postId).map(post -> {
+			post.setTitle(postRequest.getTitle());
+			post.setContent(postRequest.getContent());
+			post.setDescription(postRequest.getDescription());
+			return postRepository.save(post);
+		}).orElseThrow(() -> new ResourceNotFoundException("Post " + postId + " not found"));
 
 	}
 
 	@DeleteMapping("/posts/{postId}")
 	public ResponseEntity<?> deletePost(@PathVariable Long postId) {
-		return postRepository
-				.findById(postId)
-				.map(post -> {
-					postRepository.delete(post);
-					return ResponseEntity.ok().build();
-				})
-				.orElseThrow(
-						() -> new ResourceNotFoundException("Post " + postId
-								+ " not found"));
+		return postRepository.findById(postId).map(post -> {
+			postRepository.delete(post);
+			return ResponseEntity.ok().build();
+		}).orElseThrow(() -> new ResourceNotFoundException("Post " + postId + " not found"));
 	}
 }
