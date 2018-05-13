0ea07eb237eb4ef57dfc0987d1f394c2775090d2
unknown <maivanbao@gmail.com>
2018-05-13 22:34:51 +0700

    add todo

---
+++ easynotes/src/main/java/com/example/easynotes/controller/TodoController.java
@@ -0,0 +1,65 @@
+package com.example.easynotes.controller;
+
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.easynotes.model.Todo;
+import com.example.easynotes.repository.TodoRepository;
+
+@RestController
+@CrossOrigin("*")
+public class TodoController {
+
+	@Autowired
+	private TodoRepository todoRepository;
+
+	@GetMapping("/todos")
+	public List<Todo> getAllTodo() {
+		return todoRepository.findAll();
+	}
+
+	@PostMapping("/todos")
+	public Todo createTodo(@Valid @RequestBody Todo todo) {
+		todo.setCompleted(false);
+		return todoRepository.save(todo);
+
+	}
+
+	@GetMapping("/todos/{todoId}")
+	public ResponseEntity<Todo> getTodoById(@PathVariable(value = "todoId") Long todoId) {
+		return todoRepository.findById(todoId).map(todo -> {
+			return ResponseEntity.ok().body(todo);
+		}).orElse(ResponseEntity.notFound().build());
+	}
+
+	@PutMapping("/todos/{todoId}")
+	public ResponseEntity<Todo> updateTodo(@PathVariable(value = "todoId") Long todoId, @Valid @RequestBody Todo todoRequest) {
+		return todoRepository.findById(todoId).map(todo -> {
+			todo.setCompleted(todoRequest.getCompleted());
+			todo.setTitle(todoRequest.getTitle());
+			Todo updatedTodo = todoRepository.save(todo);
+			return ResponseEntity.ok().body(updatedTodo);
+		}).orElse(ResponseEntity.notFound().build());
+	}
+
+	@DeleteMapping(value = "/todos/{todoId}")
+	public ResponseEntity<?> deleteTodo(@PathVariable("todoId") Long id) {
+		return todoRepository.findById(id).map(todo -> {
+			todoRepository.deleteById(id);
+			return ResponseEntity.ok().build();
+		}).orElse(ResponseEntity.notFound().build());
+	}
+
+}
---
+++ easynotes/src/main/java/com/example/easynotes/model/Todo.java
@@ -0,0 +1,97 @@
+package com.example.easynotes.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+@Entity
+@Table(name="todos")
+public class Todo extends AuditModel {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+
+	
+	private String title;
+
+	private Boolean completed;
+
+	public Todo() {
+		super();
+	}
+
+	public Todo(Long id, String title, Boolean completed) {
+		super();
+		this.id = id;
+		this.title = title;
+		this.completed = completed;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public Boolean getCompleted() {
+		return completed;
+	}
+
+	public void setCompleted(Boolean completed) {
+		this.completed = completed;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((completed == null) ? 0 : completed.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((title == null) ? 0 : title.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Todo other = (Todo) obj;
+		if (completed == null) {
+			if (other.completed != null)
+				return false;
+		} else if (!completed.equals(other.completed))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (title == null) {
+			if (other.title != null)
+				return false;
+		} else if (!title.equals(other.title))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Todo [id=" + id + ", title=" + title + ", completed=" + completed + "]";
+	}
+
+}
